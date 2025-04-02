13839dc670139182a48f6390b2f86e7d87516184
Davimazoti <davilmazoti07@gmail.com>
2025-04-02 14:01:21 -0300

    'teste'

---
+++ Aula2_Back_Front/Aula2_Back_Front_Produto/src/main/java/com/example/Aula2_Back_Front_Produto/Aula2BackFrontProdutoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Aula2_Back_Front_Produto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula2BackFrontProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula2BackFrontProdutoApplication.class, args);
+	}
+
+}
---
+++ Aula2_Back_Front/Aula2_Back_Front_Produto/src/test/java/com/example/Aula2_Back_Front_Produto/Aula2BackFrontProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Aula2_Back_Front_Produto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula2BackFrontProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
